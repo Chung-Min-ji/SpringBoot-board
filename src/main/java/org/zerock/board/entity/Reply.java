1d0ad137afebe6c1f389520e92fcdb8f3322cf86
정민지 <min.dddi75@gmail.com>
2021-12-06 13:46:31 +0900

    댓글 crud(@RestController, JSON)

--- src/main/java/org/zerock/board/entity/Reply.java
+++ src/main/java/org/zerock/board/entity/Reply.java
@@ -3,15 +3,16 @@ package org.zerock.board.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@ToString(exclude = "board") // 주의***
+@ToString // ****주의
 // 회원이 아닌 사람도 댓글을 남길 수 있다고 가정하고, Board와의 연관관계를 맺지 않은 상태로 처리
-public class Reply {
+public class Reply extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,6 +22,11 @@ public class Reply {
 
     private String replyer;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude   // ****
     private Board board; // 연관관계 지정
+
+//    public void setBoard(Board board) {
+//        this.board = board;
+//    }
 }
--- src/main/java/org/zerock/board/repository/ReplyRepository.java
+++ src/main/java/org/zerock/board/repository/ReplyRepository.java
@@ -3,11 +3,19 @@ package org.zerock.board.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Reply;
 
+import java.util.List;
+
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    // Board 삭제시 댓글도 삭제
     // JPQL이용해서 update, delete 실행하려면 @Modifying 어노테이션 같이 추가해야 함
     @Modifying
     @Query("delete from Reply r where r.board.bno=:bno")
     void deleteByBno(Long bno);
+
+    // 게시물로 댓글 목록 가져오기
+    List<Reply> getRepliesByBoardOrderByRno(Board board);
 }
